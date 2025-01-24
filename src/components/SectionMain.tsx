4970ae107cfeb1438f6be2a0d3857cdad83332b9
The Lu <luis.tapia@linktic.com>
2025-01-24 14:29:10 -0400

    fix names

--- src/components/SectionMain.tsx
+++ src/components/SectionMain.tsx
@@ -5,15 +5,14 @@ import Resumen from "./contents/Resume";
 
 export default function SectionMain() {
   const [activePage, setActivePage] = useState("about");
-    const pages =[ <About/>, <Resumen/>]
+    const pages =[ {name:"about", page:<About/>}, {name:"resume", page:<Resumen/>}]
     return (
         <section className="pb-16 lg:relative lg:w-max lg:m-auto xl:min-w-[75%] xl:m-0 xl:w-[75%] overflow-hidden">
             <Nav activePage={activePage} setActivePage={setActivePage} />
 
             {pages.find(page => {
-                console.log(page.type.name)
-                return page.type.name.toLowerCase() === activePage
-            })}
+                return page.name.toLowerCase() === activePage
+            })?.page}
         </section>
     )
 }
