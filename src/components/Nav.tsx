e4f496454112267555ac054cbd45dc77c5140d01
the lu <luiseducol13@gmail.com>
2025-03-09 23:09:06 -0400

    fix bug iteracion

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav({ activePage, setActivePage }: { activePage: string,
     <nav className="fixed bottom-0 left-0 w-full bg-[#2b2b2cbf] backdrop-blur-md border border-[#383838] max-lg:rounded-t-2xl shadow-[0_16px_30px_#00000040] z-50 lg:absolute lg:bottom-auto lg:top-0 lg:left-auto lg:right-0 lg:w-max lg:rounded-bl-[20px] lg:rounded-tr-[20px] lg:px-[20px] lg:shadow-none">
       <ul className="flex flex-wrap justify-center items-center px-">
         {pages.map((page) => (
-          <li>
+          <li key={page}> {/* Agrega la prop key */}
             <button onClick={() => handlePageChange(page)} className={`text-[#d6d6d6] text-sm py-[20px] px-[8px] hover:text-transparent hover:bg-custom-radial hover:bg-clip-text focus:text-transparent focus:bg-custom-radial focus:bg-clip-text ${activePage === page ? "text-transparent bg-custom-radial bg-clip-text" : ""}`}>
               {page.charAt(0).toUpperCase() + page.slice(1)}
             </button>
